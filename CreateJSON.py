1350cb003c36cb297f5e2caf1d86069e3bd24a27
excepterror <thomas.sfondylis@gmail.com>
2025-04-24 16:43:07 +0300

    Code improvements.

--- CreateJSON.py
+++ CreateJSON.py
@@ -3,123 +3,126 @@ import re
 import json
 import requests
 import logging
+import time
 from lxml import etree
 
-# from json2html import *
+GREEN = '\033[1;32m'
+YELLOW = '\033[1;33m'
+MAGENTA = '\033[1;35m'
+RESET = '\033[0m'
 
-logging.getLogger().setLevel(logging.INFO)
-
-# with open('C:\\Users\\Thomas\\Desktop\\JSONs\\2.json', encoding='utf8') as f:
-#     d = json.load(f)
-#     e = json2html.convert(json=d)
-#     g = ("C:\\Users\\Thomas\\Desktop\\JSONs\\2.html")
-#     with open(g, "w") as file:
-#         file.write(str(e))
-#         print("yes") #to see if the code worked
-
-roster = {'ALBA Berlin': {},
-          'Anadolu Efes Istanbul': {},
-          'AS Monaco': {},
-          'EA7 Emporio Armani Milan': {},
-          'Baskonia Vitoria-Gasteiz': {},
-          'Crvena Zvezda Meridianbet Belgrade': {},
-          'Partizan Mozzart Bet Belgrade': {},
-          'FC Barcelona': {},
-          'FC Bayern Munich': {},
-          'Fenerbahce Beko Istanbul': {},
-          'LDLC ASVEL Villeurbanne': {},
-          'Maccabi Playtika Tel Aviv': {},
-          'Olympiacos Piraeus': {},
-          'Panathinaikos AKTOR Athens': {},
-          'Real Madrid': {},
-          'Valencia Basket': {},
-          'Zalgiris Kaunas': {},
-          'Virtus Segafredo Bologna': {}}
+logging.basicConfig(format='%(message)s',  level=logging.INFO)
 
 latin_num = ('I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'JR', 'Iv')
-players_count = 0
-json_files_count = 0
 
-for root, dirs, files in os.walk('C:\\Users\\Thomas\\Desktop\\JSONs\\2023-24'):
-    for file in files:
-        if file.endswith('.json'):
-            json_files_count += 1
+# --- Roster template ---
+def initialize_roster():
+    return {
+        'ALBA Berlin': {},
+        'Anadolu Efes Istanbul': {},
+        'AS Monaco': {},
+        'EA7 Emporio Armani Milan': {},
+        'Baskonia Vitoria-Gasteiz': {},
+        'Crvena Zvezda Meridianbet Belgrade': {},
+        'Partizan Mozzart Bet Belgrade': {},
+        'FC Barcelona': {},
+        'FC Bayern Munich': {},
+        'Fenerbahce Beko Istanbul': {},
+        'LDLC ASVEL Villeurbanne': {},
+        'Maccabi Playtika Tel Aviv': {},
+        'Olympiacos Piraeus': {},
+        'Panathinaikos AKTOR Athens': {},
+        'Real Madrid': {},
+        'Paris Basketball': {},
+        'Zalgiris Kaunas': {},
+        'Virtus Segafredo Bologna': {}
+    }
 
-print(f'\nJSON files to be processed: {json_files_count}\n')
+# --- Normalize player name ---
+def normalize_name(name_list):
+    fixed_name = ''
+    if len(name_list) == 2:
+        name_list = name_list[::-1]
+        fixed_name = ' '.join(_.capitalize() for _ in name_list)
+    elif len(name_list) == 3:
+        name_list = name_list[::-1]
+        for n in name_list:
+            if n in latin_num:
+                suffix = n
+                name_list.remove(n)
+                fixed_name = ' '.join(_.capitalize() for _ in name_list) + f' {suffix}'
+                break
+        else:
+            name_list[1], name_list[2] = name_list[2], name_list[1]
+            fixed_name = ' '.join(_.capitalize() for _ in name_list)
+    return fixed_name.strip()
 
-for index in range(1, json_files_count + 1):
-    with open(f'C:\\Users\\Thomas\\Desktop\\JSONs\\2023-24\\{index}.json', encoding='utf8') as f:
+# --- Construct player page URL ---
+def build_player_url(fixed_name, code):
+    dashed_name = fixed_name.lower().replace(' ', '-')
+    return f'https://www.euroleaguebasketball.net/euroleague/players/{dashed_name}/{code}/'
+
+# --- Extract photo URL from player's webpage ---
+def extract_photo_url(page_content):
+    tree = etree.HTML(page_content)
+    photo_urls = tree.xpath('//div[@class="player-hero_inner__xxqLy side-gaps_sectionSideGaps__8hmjO"]'
+                            '//div[@class="player-hero_imageWrap__OT5kR cover-contain_ofCover__PTqTb"]//img//@data-srcset')
+    if not photo_urls:
+        raise IndexError("No photo URL found")
+    photo_url = photo_urls[0].split(',')[0]
+    photo_url_fixed = re.search(r"(?P<url>https?://\S+)", photo_url).group("url")
+    return photo_url_fixed.replace('=webp', '=png')
+
+# --- Process a single JSON file ---
+def process_json_file(filepath, roster, players_count):
+    with open(filepath, encoding='utf8') as f:
         d = json.load(f)
         data = d['data']
-        for i in range(len(data)):
-            person = data[i]['person']
+        for item in data:
+            person = item['person']
             name = person['name']
             code = person['code']
-            club = data[i]['club']['name']
+            club = item['club']['name']
             split_n = re.findall(r'\w+', name)
-            name = split_n
 
-            suffix = str()
-            fixed_name = str()
-            log = bool(False)
-            if len(name) == 2:
-                name = name[::-1]
-                for _ in name:
-                    if _ not in latin_num:
-                        fixed_name += _.capitalize() + " "
-            if len(name) == 3:
-                name = name[::-1]
-                for _ in name:
-                    if _ in latin_num:
-                        suffix = _
-                        log = True
-                        fixed_name = str()
-                        break
-                if log:
-                    name.pop(name.index(suffix))
-                    for k in name:
-                        fixed_name += k.capitalize() + " "
-                    fixed_name += suffix
-                if not log:
-                    name[1], name[2] = name[2], name[1]
-                    for k in name:
-                        fixed_name += k.capitalize() + " "
-
-            fixed_name = fixed_name.rstrip()
-            dashed_name = fixed_name.lower().replace(' ', '-')
-            base_url = f'https://www.euroleaguebasketball.net/euroleague/players/{dashed_name}/{code}/'
+            fixed_name = normalize_name(split_n)
+            base_url = build_player_url(fixed_name, code)
             players_count += 1
 
             try:
                 response = requests.get(base_url, timeout=(30, 30))
-            except requests.exceptions.HTTPError as http_error:
-                logging.warning('HTTP error occurred: {}'.format(http_error))
-            except requests.exceptions.ConnectTimeout as conn_timeout:
-                logging.warning('Connection timed-out: {}'.format(conn_timeout))
+                response.raise_for_status()
+                photo_url = extract_photo_url(response.content)
             except Exception as e:
-                logging.warning(e)
-            else:
-                tree = etree.HTML(response.content)
-                photo_urls = tree.xpath('//div[@class="player-hero_inner__rwwR_ side-gaps_sectionSideGaps__v5CKj"]'
-                                        '//div[@class="player-hero_imageWrap__h_4aj cover-contain_ofCover__PuYyp"]//img//@data-srcset')
-                try:
-                    photo_url = photo_urls[0].split(',')[0]
-                except IndexError as idx_err:
-                    logging.warning(f' {fixed_name}:' + ' Photo url probably non-existent: [ERROR] {}'.format(idx_err))
-                    photo_url_fixed = ''
-                    roster[club][fixed_name] = base_url, photo_url_fixed
-                    print(roster[club][fixed_name])
-                else:
-                    photo_url_fixed = re.search(r"(?P<url>https?://\S+)", photo_url).group("url")
-                    photo_url_fixed = photo_url_fixed.replace('=webp', '=png')
-                    roster[club][fixed_name] = base_url, photo_url_fixed
-    print(f'\nJSON file {index} processed!\n')
-print(roster)
-print(f'\nPlayers processed: {players_count}')
-print(f'JSON files processed: {json_files_count}')
+                logging.warning(f'\n[{YELLOW}WARNING{RESET}] {fixed_name}: Could not fetch photo or page. [ERROR] {e}\n')
+                photo_url = ''
 
-with open('C:\\Users\\Thomas\\Desktop\\JSONs\\2023-24\\roster.json', "w") as outfile:
-    json.dump(roster, outfile, indent=4)
+            roster[club][fixed_name] = base_url, photo_url
+            logging.info(f'[{GREEN}INFO{RESET}] {club} - {fixed_name}: {base_url}, {photo_url}')
 
-# with open('roster.json') as json_file:
-#     data = json.load(json_file)
+            time.sleep(0.5)  # Optional delay
+    return players_count
+
+# --- Main routine ---
+def main():
+    directory = 'C:\\Users\\Thomas\\Desktop\\JSONs\\2024-25'
+    roster = initialize_roster()
+    players_count = 0
+    json_files = [f for f in os.listdir(directory) if f.endswith('.json')]
+
+    logging.info(f'[{GREEN}INFO{RESET}] JSON files to be processed: {len(json_files)}\n')
+
+    for idx, file in enumerate(sorted(json_files, key=lambda x: int(x.replace('.json', ''))), 1):
+        filepath = os.path.join(directory, file)
+        players_count = process_json_file(filepath, roster, players_count)
+        logging.info(f'\n[{GREEN}INFO{RESET}] {MAGENTA}JSON file {idx} processed!{RESET}\n')
+
+    logging.info(f'[{GREEN}INFO{RESET}] {YELLOW}Roster:{RESET}{roster}')
+    logging.info(f'\n[{GREEN}INFO{RESET}] {MAGENTA}Players processed:{RESET} {players_count}')
+    logging.info(f'[{GREEN}INFO{RESET}] JSON files processed: {len(json_files)}')
+
+    with open(os.path.join(directory, 'roster.json'), "w", encoding='utf8') as outfile:
+        json.dump(roster, outfile, indent=4, ensure_ascii=False)
+
+if __name__ == '__main__':
+    main()
